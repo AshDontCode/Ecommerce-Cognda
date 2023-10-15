bbe9ed34b724c83fc805193cbd1b11772ada38d4
AshDontCode <ojhaash247@gmail.com>
2023-10-15 22:45:02 +0530

    added filters

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,16 @@
 import React from 'react';
-
 import {ProductFeed} from './ProductFeed';
 import {Cart} from './Cart';
 import { Slider } from './Slider';
 import './Body.css';
 
 export const Body = (props)=>{
+    
     // const width = props.cartVisible? 'w-75':'w-100';
     const body = <div className='d-flex flex-column ' style={{width:"80%", margin:"auto"} }>
 
                     <Slider/>
-
+                    
                     <div className='d-flex'>
                     <div className="w-100">
                     <ProductFeed addToCart={props.addToCart} showCart = {props.showCart}/>
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,9 +1,27 @@
 import { useEffect, useState } from 'react';
+import React from 'react';
+
 import {ProductCard} from './ProductCard';
 import {Shimmer} from './Shimmer';
+import {filterProducts} from '../utility/filter';
 
 export const ProductFeed = (props)=>{
     const [products, setProducts] = useState([]);
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const [filters, setFilters] = useState([]);
+
+    const handleCheckboxChange = (event) => {
+        const value = event.target.value;
+    
+        if (filters.includes(value)) {
+          setFilters(prevFilters => prevFilters.filter(filter => filter !== value));
+        } else {
+          setFilters(prevFilters => [...prevFilters, value]);
+        }
+        
+    };
+
+    const visibleProducts = filters.length===0?products:filteredProducts;
 
     useEffect(()=>{
         fetch("https://fakestoreapi.com/products")
@@ -18,13 +36,26 @@ export const ProductFeed = (props)=>{
         })
     },[])
 
-    const cards = (products.length===0) ? <Shimmer/> : products.map((product)=>{
+    useEffect(()=>{
+        const updatedProducts = filterProducts(filters, products);
+        setFilteredProducts(updatedProducts);
+    },[filters,products])
+
+
+    const cards = (products.length===0) ? <Shimmer/> : visibleProducts.map((product)=>{
         return <ProductCard product={product} key={product.id} addToCart={props.addToCart} showCart={props.showCart}></ProductCard>
     })
 
     return(
+        <React.Fragment>
+        <div className='d-flex filters'>
+                        <input type='checkbox' value="men" checked={filters.includes('men')} onChange={handleCheckboxChange}/>Men's Clothing
+                        <input type='checkbox' value="women" checked={filters.includes('women')} onChange={handleCheckboxChange}/>Women's Clothing
+                        <input type='checkbox' value="four+" checked={filters.includes('four+')} onChange={handleCheckboxChange}/>4 star and above
+            </div>
         <div className="d-flex flex-wrap m-3">
             {cards}
         </div>
+        </React.Fragment>
     )
 }
